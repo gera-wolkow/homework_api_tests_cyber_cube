f5be92e47d978ae73f43ae7cdb74645c5743ab9e
iuriiryndin <georgiy.wolkow@gmail.com>
2020-04-23 12:34:13 +0300

    Added new tests

--- src/test/java/apiTests/createTests.java
+++ src/test/java/apiTests/createTests.java
@@ -1,6 +1,11 @@
 package apiTests;
 
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Date;
 
 /**
  * Created by iuriiryndin on 22.04.2020
@@ -8,7 +13,37 @@ import org.testng.annotations.Test;
 public class createTests extends setup{
 
     @Test
-    public static void test () {
-
+    public static void createDefaultUser () {
+        String userName = userData.get(userNameKey).toString();
+        String userJob = userData.get(userJobKey).toString();
+        Response response = createUser(userData);
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertNotNull(response.getBody());
+        Assert.assertEquals(response.path(userNameKey), userName);
+        Assert.assertEquals(response.path(userJobKey), userJob);
+        Assert.assertTrue(Integer.parseInt( response.path(userIdKey)) > 0 );
+//      Assert.assertTrue((Date)response.path(userCreatedAtKey) < System.currentTimeMillis());
     }
+
+    @Test (description = "Check User create in case Name element is required", enabled = false)
+    public static void createDefaultUserWithoutName () {
+        JSONObject userDataLocal = new JSONObject(userData);
+        userDataLocal.remove(userNameKey);
+        Response response = createUser(userDataLocal);
+        Assert.assertEquals(response.getStatusCode(), 400);
+        Assert.assertNull(response.getBody());
+    }
+
+    @Test (description = "Check User create in case Job element is required", enabled = false)
+    public static void createDefaultUserWithoutJob () {
+        JSONObject userDataLocal = new JSONObject(userData);
+        userDataLocal.remove(userJobKey);
+        Response response = createUser(userDataLocal);
+        Assert.assertEquals(response.getStatusCode(), 400);
+        Assert.assertNull(response.getBody());
+    }
+
+
+
+
 }
--- src/test/java/apiTests/setup.java
+++ src/test/java/apiTests/setup.java
@@ -1,14 +1,32 @@
 package apiTests;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by iuriiryndin on 22.04.2020
  */
 public class setup extends Main{
+
+    public static JSONObject userData;
+    public static String userNameKey = "name";
+    public static String userJobKey = "job";
+    public static String userIdKey = "id";
+    public static String userCreatedAtKey = "createdAt";
+    public static String userUpdatedAtKey = "updatedAt";
+
     @BeforeSuite
-    public static void healthCheck () {
+    public static void healthCheck () throws IOException, ParseException {
         Assert.assertEquals(getUsers().getStatusCode(), 200);
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(new File("./src/test/resources/user.json")));
+        userData = (JSONObject) obj;
     }
 }
